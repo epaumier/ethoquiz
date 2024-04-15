fae7bcb3498a8de8c8b89ed98df01b739e0f1074
edouardp <contact@paumier-edouard.com>
2024-04-15 23:35:58 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Pressable, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  <View className="flex-1 items-center justify-around bg-white">
+      <Text className="text-2xl bg-green-800 px-8 py-4 text-white rounded-lg">Étho Quiz</Text>
+      <View className="flex flex-col gap-4">
+        <Pressable>
+          <Text className="text-xl text-white bg-green-800 rounded-lg px-10 py-3">Démarrer</Text>
+        </Pressable>
+        <Pressable>
+          <Text className="text-xl text-white bg-green-800 rounded-lg px-10 py-3">À propos</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
